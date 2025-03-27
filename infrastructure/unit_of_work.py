154064330606b66fcad341cd7067d29a6851fd98
Pavel Mayorov <pmayorov@cloudlinux.com>
2025-03-27 23:33:27 +0300

    SqlAlchemyUnitOfWork implementation

--- infrastructure/unit_of_work.py
+++ infrastructure/unit_of_work.py
@@ -1,18 +1,25 @@
 from domain.unit_of_work import UnitOfWork
 
+
 class SqlAlchemyUnitOfWork(UnitOfWork):
 
     def __init__(self, session):
-        pass
+        self.session = session
+        self.committed = False
 
     def __enter__(self):
-        pass
+        return self
 
     def __exit__(self, exception_type, exception_value, traceback):
-        pass
+        if exception_type is None and not self.committed:
+            self.commit()
+        elif exception_type is not None:
+            self.rollback()
 
     def commit(self):
-        pass
+        self.session.commit()
+        self.committed = True
 
     def rollback(self):
-        pass
+        self.session.rollback()
+        self.committed = True
