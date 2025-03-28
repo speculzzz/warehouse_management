9a242d7b5883ae0aed53286e64a34603e4b3d446
Pavel Mayorov <pmayorov@cloudlinux.com>
2025-03-28 19:18:39 +0300

    Improve SqlAlchemyUnitOfWork implementation

--- infrastructure/unit_of_work.py
+++ infrastructure/unit_of_work.py
@@ -1,25 +1,45 @@
+from sqlalchemy.exc import SQLAlchemyError
 from domain.unit_of_work import UnitOfWork
 
 
 class SqlAlchemyUnitOfWork(UnitOfWork):
 
     def __init__(self, session):
-        self.session = session
-        self.committed = False
+        self._session = session
+        self._savepoint = []
 
     def __enter__(self):
+        lvl = len(self._savepoint) + 1
+        print(f"[BEGIN_NESTED {lvl}]")
+        session = self._session.begin_nested()
+        self._savepoint.append((lvl, session))
         return self
 
-    def __exit__(self, exception_type, exception_value, traceback):
-        if exception_type is None and not self.committed:
-            self.commit()
-        elif exception_type is not None:
-            self.rollback()
+    def __exit__(self, exception_type, exception_value, exception_traceback):
+        print("[EXIT]")
+        try:
+            if exception_type is None:
+                self.commit()
+            else:
+                self.rollback()
+        except SQLAlchemyError as exc:
+            raise RuntimeError(f"Transaction failed: {str(exc)}") from exc
 
     def commit(self):
-        self.session.commit()
-        self.committed = True
+        if self._savepoint:
+            lvl, session = self._savepoint.pop()
+            print(f"[COMMIT {lvl}]")
+            session.commit()
+        else:
+            print("[COMMIT]")
+            self._session.commit()
+
 
     def rollback(self):
-        self.session.rollback()
-        self.committed = True
+        if self._savepoint:
+            lvl, session = self._savepoint.pop()
+            print(f"[ROLLBACK {lvl}]")
+            session.rollback()
+        else:
+            print("[ROLLBACK]")
+            self._session.rollback()
